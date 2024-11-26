0c57e4f161f93454489f45c15dceba8630139831
JuanPaManjarres <jupamanj@espol.edu.ec>
2024-11-25 22:27:39 -0500

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -11,16 +11,4 @@ class Tablero:
   def seleccionarCarta(self,indice):
     print(self._str_())
     return self.__cartasjugador[1,indice]#Retorna la Carta del indice indicado
-  def agregarCartaTablero(self,carta):
-      if isinstance(carta,CartaMonstruo):
-        if None in self.__cartasjugador[0]:
-          indice= self.__cartasjugador[0].index(None)
-          self.__cartasjugador[0][indice]= carta
-        else:
-          print("Espacio para carta tipo Monstruo lleno en el tablero")
-      elif isinstance(carta,CartaMagica) or isinstance(carta,CartaMonstruo):
-        if None in self.__cartasjugador[1]:
-          indice= self.__cartasjugador[1].index(None)
-          self.__cartasjugador[1][indice]= carta
-        else:
-          print("Espacio para cartas tipo Magica o Trampa lleno en el tablero")
+  
